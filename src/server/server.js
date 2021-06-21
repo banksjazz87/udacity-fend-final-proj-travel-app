ec6217dc24eb80c02138cf9ee393d5dcebd9a389
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-06-21 05:56:31 -0400

    chore: I'm working on retrieving the data from .env file.

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3080;
 
+//setting up an instance of .env
+const dotenv = require('dotenv');
+dotenv.config();
+
+const keys = {
+    url = process.env.GEONAMES_HOST
+}
+
+console.log(process.env.GEONAMES_HOST);
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
