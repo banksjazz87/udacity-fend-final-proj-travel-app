a33f3ba81fb92df3a8e6c4c31a7371c92fee52de
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-06-26 07:51:33 -0400

    chore: I added the cleanWebpackPlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -21,8 +22,16 @@ module.exports = {
         }
     ]
     },
-    plugins: [new HtmlWebPackPlugin({
+    plugins: [
+    new HtmlWebPackPlugin({
         template: "./src/client/html/views/index.html", 
         filename: "./index.html"
-    })],
+    }), 
+    new CleanWebpackPlugin({
+        dry: true,
+        verbose: true, 
+        cleanStaleWebpackAssets: true,
+        protectWebpackAssets: false
+    })
+    ],
 };
