035a0d8e43ab7ac294cac21c6ca5982ba8b44be0
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-07-06 05:51:51 -0400

    chore: I have the geodata returning the information that will be displayed to the user

--- src/client/js/geoCall.js
+++ src/client/js/geoCall.js
@@ -8,9 +8,30 @@ async function geonamesApi(key) {
     try {
         const res = await (response.json());
         console.log(res);
+        displayedGeo(res.geonames);
     } catch (e) {
         console.log(error);
     }
 }
 
 export { geonamesApi }
+
+//function to return the info from the geonamesApi to the DOM
+
+function displayedGeo(data) {
+    const select = document.createElement('select');
+    const selectContainer = document.getElementById('select_location');
+    selectContainer.appendChild(select);
+
+    let arr = [];
+
+    for (let i = 0; i < data.length; i++) {
+        let place = "Place: " + Object.values(data[i].name).join('') + ",";
+        let state = "State: " + Object.values(data[i].adminName1).join('') + ",";
+        let country = "Country: " + Object.values(data[i].countryName).join('');
+
+        arr.push(place + " " + state + " " + country);
+    }
+    console.log(arr);
+
+}
