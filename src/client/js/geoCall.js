61da387e0c11925c6730b7e5671a02f7dc04b8e3
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-07-06 22:29:15 -0400

    chore: I have created an output to the UI after the user clicks on 'enter', I now need to add an event listener for the options that are being displayed

--- src/client/js/geoCall.js
+++ src/client/js/geoCall.js
@@ -26,9 +26,20 @@ function displayedGeo(data) {
     let arr = [];
 
     for (let i = 0; i < data.length; i++) {
-        let place = "Place: " + Object.values(data[i].name).join('') + ",";
-        let state = "State: " + Object.values(data[i].adminName1).join('') + ",";
-        let country = "Country: " + Object.values(data[i].countryName).join('');
+        let place = "LOCATION: " + Object.values(data[i].name).join('');
+        let state = "STATE: " + Object.values(data[i].adminName1).join('') + ",";
+        let country = "COUNTRY: " + Object.values(data[i].countryName).join('');
+
+        let options = document.createElement('option');
+        options.textContent = place + " " + state + " " + country;
+
+        select.appendChild(options);
+
+
+        //The below code doesn't work, because the options don't actually exist in the dom, will need to run another loop and apply the even listener
+        options.addEventListener('click', (e) => {
+            console.log(e.target.value);
+        })
 
         arr.push(place + " " + state + " " + country);
     }
