305d47997d6c6b038232216ff9b177b460895730
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-07-27 17:18:12 -0400

    feature: I am still working on getting the data from the pixabay api to update the UI

--- src/client/js/pixabayCall.js
+++ src/client/js/pixabayCall.js
@@ -2,6 +2,8 @@
 const pixCall = () => {
     MyLib.keysInfo()
         .then(data => pixabayApi(data.pixabay, data.pixabayUrl, MyLib.newOptions.place))
+        // .then(data => checkForZero(data.totalHits))
+        //.then(data => pixabayPic(data.hits[0][fullHDURL]))
 
 }
 
@@ -22,10 +24,21 @@ const pixabayApi = async(key, url, place) => {
     }
 }
 
-//Return an image
+//Check to see if no image has been found for the location.
 const checkForZero = (value) => {
     if (value === 0) {
         MyLib.keysInfo()
             .then(data => pixabayApi(data.pixabay, data.pixabayUrl, MyLib.newOptions.country))
+            //.then(data => pixabayPic(data.hits[0][fullHDURL]))
     }
+}
+
+//Function to return an image to the UI
+const pixabayPic = (value) => {
+    const container = document.getElementById('location_pic');
+
+    const pic = document.createElement('img');
+    pic.setAttribute('src', value);
+
+    container.appendChild(pic);
 }
