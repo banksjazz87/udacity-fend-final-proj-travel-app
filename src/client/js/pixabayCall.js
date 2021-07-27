08f5f949ef0acb95ee6382c179bb729a11913a66
banksjazz87 <61635862+banksjazz87@users.noreply.github.com>
2021-07-27 05:55:32 -0400

    feature: I am working on returning an image with the pixabay api, and also creating a fall back if no image is returned

--- src/client/js/pixabayCall.js
+++ src/client/js/pixabayCall.js
@@ -1,20 +1,31 @@
-const pixabayApi = async(key, url, value) => {
+//Function that returns a series of other functions to ultimately return an image to the UI
+const pixCall = () => {
+    MyLib.keysInfo()
+        .then(data => pixabayApi(data.pixabay, data.pixabayUrl, MyLib.newOptions.place))
+
+}
+
+export { pixCall }
+
+const pixabayApi = async(key, url, place) => {
 
     //let description = arr.join('+').toString();
 
-    const response = await fetch(url + key + "&q=" + value + "&image_type=photo");
+    const response = await fetch(url + key + "&q=" + place + "&image_type=photo");
 
     try {
         let data = await response.json();
         console.log(data);
+        return data;
     } catch (e) {
         console.log('error', e);
     }
 }
 
-const pixCall = () => {
-    MyLib.keysInfo()
-        .then(data => pixabayApi(data.pixabay, data.pixabayUrl, MyLib.newOptions.place))
+//Return an image
+const checkForZero = (value) => {
+    if (value === 0) {
+        MyLib.keysInfo()
+            .then(data => pixabayApi(data.pixabay, data.pixabayUrl, MyLib.newOptions.country))
+    }
 }
-
-export { pixCall }
